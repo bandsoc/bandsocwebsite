6f1c936475d5db03d3f783bb92f06f71b8594e30
Bora Akyuz <bora.akyuz@warwick.ac.uk>
2022-08-27 22:15:49 +0100

    change stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
-import theme from './theme'
+import { ChakraProvider, theme } from '@chakra-ui/react'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
