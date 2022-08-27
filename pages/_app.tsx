61798c48ed487da8a0e1b22ca1d3aa55ae695343
Bora Akyuz <bora.akyuz@warwick.ac.uk>
2022-08-27 20:44:21 +0100

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '../components/Layout'
-import theme from "./api/theme";
+import theme from './api/theme'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
