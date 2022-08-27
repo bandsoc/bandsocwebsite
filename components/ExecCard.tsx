4e6c2a7af12fe1a62df459dd3a64882facced089
Bora Akyuz <bora.akyuz@warwick.ac.uk>
2022-08-27 15:57:19 +0100

    change border color

--- components/ExecCard.tsx
+++ components/ExecCard.tsx
@@ -14,7 +14,7 @@ const ExecCard = ({ image, imageAlt, name, role, description }: IProps) => {
 
 
     return (
-        <Box maxW='20rem' borderWidth='4px' borderRadius='lg' minH='30rem'>
+        <Box maxW='20rem' borderWidth='4px' borderRadius='lg' minH='30rem' borderColor='grey.700'>
 
             <Center>
                 <Image src={image} alt={imageAlt} borderRadius='full' boxSize='180px' mt='1rem'  />
